29b4417db64b49f397c0e3204ddf03932c35d1b8
Justin Phung <justin.phung@mail.utoronto.ca>
2020-10-25 17:36:26 -0400

    Added Show/Hide comments functionality

--- phase1/src/ForumComponents/Forum/Forum.js
+++ phase1/src/ForumComponents/Forum/Forum.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ForumGroups from '../ForumGroups/ForumGroups.js';
 import ForumHeader from '../ForumHeader/ForumHeader.js';
 import ForumMenu from '../ForumMenu/ForumMenu.js';
+import Toggle from '../Toggle/Toggle.js'
 import "./Forum.css";
 
 class Forum extends React.Component {
@@ -20,6 +21,28 @@ class Forum extends React.Component {
                 <div className="forumPostBody">
                     <h1>Latest Posts <button className="button">New Post</button></h1>
                     <div className="forumBody">
+                        
+                        {/* ToDO: */}
+
+                        {/* Add functionality to the New Post Button */}
+
+                        {/* Add an option to comment on each Post */}
+
+                        {/* Add functioanlity to the Groups section of the page */}
+
+                        {/* Remember to create another view dedicated to the Admin
+                         (i.e. the ability to delete posts) */}
+
+                        {/* Add in a ForumPost Component Later it should take in
+                        3 variables: postTitle, postUser, and postContent. Don't forget
+                        each post requires a timestamp along with it */}
+                        
+                        {/* Add in a PostComment Component later it should also take in
+                        2 variables: commentUser, and commentContent. Don't forget each post
+                        requires a timestamp along with it*/}
+
+                        {/* Add in the page redirects */}
+
                         {/* Forum Post 1 */}
                         <div className="forumPost">
                             <h4 className="postTitle">Introduction Post</h4>
@@ -27,7 +50,9 @@ class Forum extends React.Component {
                             <p className="postContent">Hi everybody I am new to these forums and wanted to introduce myself. 
                             I am looking for a game to join, I haven't played but I am willing to learn. Hope to hear from somebody soon! 
                             </p>
-                            <h4 className="postComments">0 Comments</h4>
+                            <Toggle title="Show Comments">
+                                Hello
+                            </Toggle>
                         </div>
 
                         {/* Forum Post 2 */}
@@ -37,7 +62,9 @@ class Forum extends React.Component {
                            <p className="postContent">Hi everybody I am currently looking for a game to join. I have experience playing
                           Dungeon's and Dragon's so I can hope right in. Hope to hear from you guys soon!
                          </p>
-                         <h4 className="postComments">0 Comments</h4>
+                        <Toggle title="Show Comments">
+                            Hello
+                        </Toggle>
                         </div>
                     </div>
                 </div>
