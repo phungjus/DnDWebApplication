0ed14e35ac2abfe218c1d6fe5c04ea04e308b970
Jessica Glustien <jessica.glustien@mail.utoronto.ca>
2020-10-25 21:12:06 -0400

    Added ability to upload character picture file from computer

--- phase1/src/character-creation-page/character-upload.jsx
+++ phase1/src/character-creation-page/character-upload.jsx
@@ -4,55 +4,87 @@ import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import './character-upload-styles.css'
 
-const CharacterUpload = () => (
-    <div className='character-upload'>
-        <img className='character-image' src = { character }/>
-        <div className = 'side-bar'>
-        <Button variant="contained" color="primary" component="span">
-          Upload Image
-        </Button>
-        </div>
-        <div className = 'side-bar'>
-        <TextField
-          id="filled-multiline-static"
-          className='input'
-          label="Personality Traits"
-          multiline
-          rows={4}
-          variant="filled"
-        />
-        </div>
-        <div className = 'side-bar'>
-        <TextField
-          id="filled-multiline-static"
-          className='input'
-          label="Ideals"
-          multiline
-          rows={4}
-          variant="filled"
-        />
-        </div>
-        <div className = 'side-bar'>
-        <TextField
-          id="filled-multiline-static"
-          className='input'
-          label="Bonds"
-          multiline
-          rows={4}
-          variant="filled"
-        />
-        </div>
-        <div className = 'side-bar'>
-        <TextField
-          id="filled-multiline-static"
-          className='input'
-          label="Flaws"
-          multiline
-          rows={4}
-          variant="filled"
-        />
-        </div>
-    </div>
-);
+class CharacterUpload extends React.Component {
+    state = {
+        selectedPic: character // default pic 
+    }
+
+    picSelectedHandler = e => {
+        const reader = new FileReader();
+        reader.onload = () => {
+            if (reader.readyState === 2) {
+                this.setState({selectedPic: reader.result})
+            }
+        }
+        reader.readAsDataURL(e.target.files[0])
+    }
+
+    render() {
+        return (
+            <div className='character-upload'>
+                <img className='character-image' src = { this.state.selectedPic }/>
+                <div className = 'side-bar'>
+                    <input
+                    accept="image/*"
+                    className="pic-upload"
+                    id="contained-button-file"
+                    type="file"
+                    onChange={this.picSelectedHandler}
+                />
+                <label htmlFor="contained-button-file">
+                    <Button variant="contained" color="primary" component="span">
+                    Upload Picture 
+                    </Button>
+                </label>
+                    </div>
+                    <div className = 'side-bar'>
+                    <TextField
+                    className='input'
+                    label="Character Name"
+                    variant="filled"
+                    />
+                    </div>
+                    <div className = 'side-bar'>
+                    <TextField
+                    className='input'
+                    label="Personality Traits"
+                    multiline
+                    rows={4}
+                    variant="filled"
+                    />
+                    </div>
+                    <div className = 'side-bar'>
+                    <TextField
+                    className='input'
+                    label="Ideals"
+                    multiline
+                    rows={4}
+                    variant="filled"
+                    />
+                    </div>
+                    <div className = 'side-bar'>
+                    <TextField
+                    className='input'
+                    label="Bonds"
+                    multiline
+                    rows={4}
+                    variant="filled"
+                    />
+                    </div>
+                    <div className = 'side-bar'>
+                    <TextField
+                    className='input'
+                    label="Flaws"
+                    multiline
+                    rows={4}
+                    variant="filled"
+                    />
+                </div>
+            </div>
+        );
+    }
+}
 
 export default CharacterUpload;
+
+
