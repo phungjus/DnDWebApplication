efd2ec4733c6e2e0ad4baf53a4e38789907894ce
Jessica Glustien <jessica.glustien@mail.utoronto.ca>
2020-10-26 01:16:03 -0400

    Completed alignment section of page

--- phase1/src/character-creation-page/character-creation-page.jsx
+++ phase1/src/character-creation-page/character-creation-page.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import CharacterUpload from './character-upload.jsx'
+import Alignment from './alignment.jsx'
+import './character-creation-page-styles.css'
 
 const CharacterCreatePage = () => (
+  <div>
+  <div class='title'><h1 class='title'>LET'S MAKE A CHARACTER</h1></div>
   <div className='charactercreatepage'>
     <CharacterUpload/>
+    <Alignment/>
+  </div>
   </div>
 );
 
