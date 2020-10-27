349ed7800e220455a85b1a65c99ad05205e86a2a
Kyoji <kyoji.goto@mail.utoronto.ca>
2020-10-27 16:13:42 -0400

    Created first draft of member list

---
+++ phase1/src/Components/Member/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './styles.css';
+
+class Member extends React.Component {
+    render() {
+        return(
+            <div className="Member">
+                <span className="dot"></span>
+                <p>
+                    {this.props.name}
+                    {" (" + this.props.memberType + ")"}
+                </p>
+                <a href="">
+                    Character sheet
+                </a>
+            </div>
+        );
+    }
+}
+
+export default Member;
---
+++ phase1/src/Components/Memberlist/index.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import './styles.css';
+import Member from '../Member'
+import PopUp from '../Groupinvite'
+
+class Memberlist extends React.Component {
+    
+    state = {
+        members : [
+            {name: "Matt", memberType: "DM"},
+            {name: "Kyoji", memberType: "Player"},
+            {name: "Jess", memberType: "Player"},
+            {name: "Justin", memberType: "Player"},
+            {name: "Kai", memberType: "Player"},
+            {name: "Maddy", memberType: "Player"},
+            {name: "Ma", memberType: "Player"},
+            {name: "Pa", memberType: "Player"},
+            {name: "Ricky", memberType: "Player"},
+            {name: "Ishan", memberType: "Player"},
+            {name: "Shakir", memberType: "Player"},
+        ]
+    }
+
+    togglePop = () => {
+        this.setState({
+         seen: !this.state.seen
+        });
+    };
+
+    startLeaveEvent = () => {
+        if(window.confirm("Are you sure you want to leave the group?")){
+            console.log('okay')
+        } else {
+            console.log('no')
+        }
+    }
+    
+    render() {
+        const members = this.state.members.map((member) => 
+            <li>
+                <Member
+                    name={member.name}
+                    memberType={member.memberType}
+                />
+            </li>)
+        return (
+            <div className="Memberlist">
+                <ul>
+                    <h1>
+                        Members
+                    </h1>
+                    <div className="Members">
+                        {members}
+                    </div>
+                    <li>
+                        <button onClick={this.togglePop}>
+                            Invite to group
+                        </button>
+                    </li>
+                    <li>
+                        <button onClick={this.startLeaveEvent}>
+                            Leave group
+                        </button>
+                    </li>
+                </ul>
+                {this.state.seen ? <PopUp toggle={this.togglePop} /> : null}
+            </div>
+        );
+   }
+}
+
+export default Memberlist;
