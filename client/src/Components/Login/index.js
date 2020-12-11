594b1c76878a0e5786e1a13af48850ac514abb5c
Kyoji <kyoji.goto@mail.utoronto.ca>
2020-12-11 07:50:28 -0500

    Fixed Login Issue

--- client/src/Components/Login/index.js
+++ client/src/Components/Login/index.js
@@ -71,8 +71,7 @@ class Login extends React.Component {
     handleLogin = (e) => {
         // Handle login in a backend server
         login(this.state.username, this.state.pass, (user) => {
-            console.log(user === "undefined")
-            if (user !== "undefined") {
+            if (typeof user !== "undefined") {
                 this.setState({
                     signin_error: null
                 })
